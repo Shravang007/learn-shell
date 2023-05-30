e0323266369c9bf7e6212efe5febb97a9e1db2a1
Shravang007 <shravangoudgajula@gmail.com>
2023-05-30 21:10:09 +0530

    Family

--- 01.print.sh
+++ 01.print.sh
@@ -2,9 +2,9 @@ echo Hello World
 
 echo -e "\e[31mShravan Kumar\e[0m"
 echo -e "\e[32mPavan Kumar\e[0m"
-echo -e "\e[33m Venkateshwarlu\e[0m"
+echo -e "\e[33mVenkateshwarlu\e[0m"
 echo -e "\e[34mpadma\e[0m"
-echo -e "\e[35m Pichamma\e[0m"
-echo -e "\e[36m Shravani\e[0m"
+echo -e "\e[35mPichamma\e[0m"
+echo -e "\e[36mShravani\e[0m"
 
 Shravan Kumar
